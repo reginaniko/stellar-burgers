53bf96fccc9cb5410b2f9200a418432f1573d6b5
Regina Nikogosian <reginanikogosian@yandex.ru>
2025-11-12 19:18:30 -0500

    chore: config fix

--- webpack.config.js
+++ webpack.config.js
@@ -83,7 +83,9 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: '/',
+    clean: true
   },
   devServer: {
     static: path.join(__dirname, './dist'),
