bab8e588585c30d0e6ffab06c4bef2276c9e9b96
Regina Nikogosian <reginanikogosian@yandex.ru>
2025-11-15 14:49:15 -0500

    cicd[SB-004]: unify test reporting, add Cypress HTML & coverage, align Pages deploy paths

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,30 @@
 import { defineConfig } from 'cypress';
+import codeCoverageTask from '@cypress/code-coverage/task';
 
 export default defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: false, 
+    json: true, 
+    charts: true,
+    reportPageTitle: 'Cypress Test Report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
+
   e2e: {
     baseUrl: 'http://localhost:4000',
+
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    }
-  }
+      codeCoverageTask(on, config);
+      require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
+    },
+
+    video: false,
+    screenshotOnRunFailure: true,
+  },
 });
